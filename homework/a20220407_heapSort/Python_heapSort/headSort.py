e3049f577577bbc4ebae78a7d7c62677ffcbe4a8
dotponder <dotponder@outlook.com>
2022-04-07 21:57:23 +0800

    update heapSort

---
+++ homework/a20220407_heapSort/Python_heapSort/headSort.py
@@ -0,0 +1,37 @@
+def build_max_heap(arr):
+    for i in range(len(arr) // 2, -1, -1):
+        heapify(arr, i)
+
+
+def heapify(arr, i):
+    left = 2 * i + 1
+    right = 2 * i + 2
+    largest = i
+    if left < arrLen and arr[left] > arr[largest]:
+        largest = left
+    if right < arrLen and arr[right] > arr[largest]:
+        largest = right
+
+    if largest != i:
+        swap(arr, i, largest)
+        heapify(arr, largest)
+
+
+def swap(arr, i, j):
+    arr[i], arr[j] = arr[j], arr[i]
+
+
+def heapSort(arr):
+    global arrLen
+    arrLen = len(arr)
+    build_max_heap(arr)
+    for i in range(len(arr) - 1, 0, -1):
+        swap(arr, 0, i)
+        arrLen -= 1
+        heapify(arr, 0)
+    return arr
+
+
+if __name__ == '__main__':
+    arr = [100, 16, 45, 46, 34, 75, 12, 3, 91, 10, 5]
+    print(heapSort(arr))
